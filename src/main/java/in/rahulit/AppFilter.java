c6a9034f6246918dca460589acfd8026a7548e10
Shahbaj007 <shahbajshaikh1997@gmail.com>
2024-08-11 22:28:42 +0530

    first commit

---
+++ src/main/java/in/rahulit/AppFilter.java
@@ -0,0 +1,39 @@
+package in.rahulit;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class AppFilter implements GlobalFilter {
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		
+		System.out.println("Filter Methods Called....");
+		
+		ServerHttpRequest request = exchange.getRequest();
+		
+		HttpHeaders headers = request.getHeaders();
+		
+		Set<String> keySet = headers.keySet();
+		
+		keySet.forEach(key -> {
+			List<String> values = headers.get(key);
+			System.out.println(key);
+			System.out.println(values);
+			System.out.println("===============================");
+		});
+		
+		return chain.filter(exchange);
+	}
+
+}
